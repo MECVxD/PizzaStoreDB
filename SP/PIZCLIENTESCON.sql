9de1e3157edd6131745ca9e4a25cd30c8ee8a64f
Mauricio Covarrubias <mauricioecovarrubias@hotmail.com>
2021-06-25 13:54:55 -0500

    Update PIZCLIENTESCON SP

--- SP/PIZCLIENTESCON.sql
+++ SP/PIZCLIENTESCON.sql
@@ -2,20 +2,16 @@
 -- Routine DDL
 -- Note: comments before and after the routine body will not be stored by the server
 -- ---------------------------------------------------------------------------------
--- SP_MAURICIOPRO
+-- PIZCLIENTESCON
 DELIMITER ;
-DROP PROCEDURE IF EXISTS `SP_MAURICIOPRO`;
+DROP PROCEDURE IF EXISTS `PIZCLIENTESCON`;
 DELIMITER $$
 
-CREATE PROCEDURE `SP_MAURICIOPRO`(
-/* SP para realizar suma de los dias*/
+CREATE PROCEDURE `PIZCLIENTESCON`(
+/* SP para realizar consulta de clientes*/
 
-	Par_FechaInicio             DATE,        	 
-	Par_NumDias					INT(2),
-
-	Par_Salida					CHAR(1), 		-- Indica mensaje de Salida
-	INOUT Par_NumErr			INT(11),		-- Numero de Error
-	INOUT Par_ErrMen			VARCHAR(400),	-- Descripcion de Error
+	Par_ClienteID               INT(11),        -- ID del Cliente
+	Par_NumCon					TINYINT UNSIGNED,
 
 	Aud_EmpresaID				INT(11),		-- Parametro de Auditoria
 	Aud_Usuario					INT(11),		-- Parametro de Auditoria
@@ -28,22 +24,21 @@ CREATE PROCEDURE `SP_MAURICIOPRO`(
 )
 TerminaStore: BEGIN
 	-- Declaracion de Constantes
-	DECLARE Cadena_Vacia			CHAR(1);		-- Constante cadena vacia ''
-	DECLARE Entero_Cero				INT(1);			-- Constante Entero cero 0
-	DECLARE Fecha_Vacia				DATE;
-	DECLARE SalidaSi				CHAR(1);
+	DECLARE Cadena_Vacia		CHAR(1);		-- Constante cadena vacia ''
+	DECLARE Entero_Cero			INT(1);			-- Constante Entero cero 0
+	DECLARE Cons_NO					CHAR(1);
+	DECLARE Cons_Principal			INT(11);
 
 	SET Entero_Cero					:= 0;
 	SET Cadena_Vacia				:= 'S';
-	SET Fecha_Vacia					:= '1900-01-01';
-	SET SalidaSi					:= 'S';
+	SET Cons_NO						:= 'S';
+	SET Cons_Principal				:= 1;
 
-	ManejoErrores:BEGIN
-
-		IF(Par_NumDias < 5) THEN
-			SELECT DATE_ADD(Par_FechaInicio, INTERVAL Par_NumDias DAY) AS FEHCA
-			ELSE 
-			SET Par_ErrMen='El numero de dias es mayor a 5'
-		END IF;
-	END ManejoErrores;
+ 	IF(Par_NumCon = Cons_Principal) THEN
+		SELECT
+			ClienteID,		NombreCompleto,			RFC,			Direccion,			Telefono
+			FROM PIZCLIENTES
+				WHERE
+				ClienteID = Par_ClienteID;
+	END IF;
 END TerminaStore$$
