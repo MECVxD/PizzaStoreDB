36b4f605e935219bdb6466965d08d0c1712416f1
Mauricio Covarrubias <mauricioecovarrubias@hotmail.com>
2021-06-25 15:35:00 -0500

    Update PIZCLIENTESCON.sql

--- SP/PIZCLIENTESCON.sql
+++ SP/PIZCLIENTESCON.sql
@@ -10,35 +10,33 @@ DELIMITER $$
 CREATE PROCEDURE `PIZCLIENTESCON`(
 /* SP para realizar consulta de clientes*/
 
-	Par_ClienteID               INT(11),        -- ID del Cliente
-	Par_NumCon					TINYINT UNSIGNED,
+	Par_ClienteID           	INT(11),        	-- ID del Cliente
+	Par_NumCon			TINYINT UNSIGNED,
 
-	Aud_EmpresaID				INT(11),		-- Parametro de Auditoria
-	Aud_Usuario					INT(11),		-- Parametro de Auditoria
-	Aud_FechaActual				DATETIME,		-- Parametro de Auditoria
-	Aud_DireccionIP				VARCHAR(15),	-- Parametro de Auditoria
-	Aud_ProgramaID				VARCHAR(50),	-- Parametro de Auditoria
-	Aud_Sucursal				INT(11),		-- Parametro de Auditoria
-	Aud_NumTransaccion			BIGINT(20)		-- Parametro de Auditoria
+	Aud_EmpresaID			INT(11),		-- Parametro de Auditoria
+	Aud_Usuario			INT(11),		-- Parametro de Auditoria
+	Aud_FechaActual			DATETIME,		-- Parametro de Auditoria
+	Aud_DireccionIP			VARCHAR(15),		-- Parametro de Auditoria
+	Aud_ProgramaID			VARCHAR(50),		-- Parametro de Auditoria
+	Aud_Sucursal			INT(11),		-- Parametro de Auditoria
+	Aud_NumTransaccion		BIGINT(20)		-- Parametro de Auditoria
 
 )
 TerminaStore: BEGIN
 	-- Declaracion de Constantes
 	DECLARE Cadena_Vacia		CHAR(1);		-- Constante cadena vacia ''
-	DECLARE Entero_Cero			INT(1);			-- Constante Entero cero 0
-	DECLARE Cons_NO					CHAR(1);
-	DECLARE Cons_Principal			INT(11);
+	DECLARE Entero_Cero		INT(1);			-- Constante Entero cero 0
+	DECLARE Cons_NO			CHAR(1);
+	DECLARE Cons_Principal		INT(11);
 
-	SET Entero_Cero					:= 0;
-	SET Cadena_Vacia				:= 'S';
-	SET Cons_NO						:= 'S';
-	SET Cons_Principal				:= 1;
+	SET Entero_Cero			:= 0;
+	SET Cadena_Vacia		:= 'S';
+	SET Cons_NO			:= 'S';
+	SET Cons_Principal		:= 1;
 
  	IF(Par_NumCon = Cons_Principal) THEN
-		SELECT
-			ClienteID,		NombreCompleto,			RFC,			Direccion,			Telefono
-			FROM PIZCLIENTES
-				WHERE
-				ClienteID = Par_ClienteID;
+		SELECT ClienteID, NombreCompleto, RFC, Direccion, Telefono
+		FROM PIZCLIENTES
+		WHERE ClienteID = Par_ClienteID;
 	END IF;
 END TerminaStore$$
